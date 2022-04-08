ce3a6bee576ae1782b501832cc3fccd88bdf3887
hoshianaaa <hoshina54321@gmail.com>
2022-04-08 13:05:42 +0900

    chage image

--- src/tempmatch.cpp
+++ src/tempmatch.cpp
@@ -4,11 +4,26 @@ using namespace cv;
 
 int main(int argc, const char * argv[])
 {
-    Mat s_img = imread( "images/tube.png" );
-    Mat t_img = imread( "images/tube_temp.png" );
+    Mat simg = imread( "images/tube.png" );
+    Mat timg = imread( "images/tube_temp.png" );
 
-    imshow("s image", s_img );
-    imshow("t image", t_img );
+    Mat d_simg = simg.clone();
+    Mat d_timg = timg.clone();
+
+    int n = 2;
+
+    for(int i=0;i<n;i++)
+    {
+      cv::pyrDown( d_simg, d_simg, cv::Size( d_simg.cols/2, d_simg.rows/2 ));
+      cv::pyrDown( d_timg, d_timg, cv::Size( d_timg.cols/2, d_timg.rows/2 ));
+    }
+
+
+    imshow("s image", simg );
+    imshow("t image", timg );
+
+    imshow("down s image", d_simg );
+    imshow("down t image", d_timg );
 
     waitKey(0);
 
