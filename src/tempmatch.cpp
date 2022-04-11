9a89fa083c5aa07fd186d8275d6f4ae1ef911723
hoshianaaa <hoshina54321@gmail.com>
2022-04-11 13:02:17 +0900

    get center

--- src/tempmatch.cpp
+++ src/tempmatch.cpp
@@ -6,6 +6,34 @@ int main(int argc, const char * argv[])
 {
     Mat simg = imread( "images/tube.png" );
     Mat timg = imread( "images/tube_temp.png" );
+    
+    int x_sum = 0;
+    int y_sum = 0;
+
+    int width = timg.cols;
+    int height = timg.rows;
+
+    int noOfCordinates = 0;
+    cv::Point *coordinates;
+    coordinates =  new cv::Point[width * height];
+
+    for (int j = 0; j < timg.rows; j++)
+    {
+        unsigned char *src = timg.ptr<unsigned char>(j);
+        for (int i = 0; i < timg.cols; i++)
+        {
+            if(src[i] == 255){
+              coordinates[noOfCordinates].x = i;
+              coordinates[noOfCordinates].y = j;
+              x_sum += i;
+              y_sum += j;
+              noOfCordinates++;
+            } 
+        }
+    }
+
+    double center_x = double(x_sum) / noOfCordinates;
+    double center_y = double(y_sum) / noOfCordinates;
 
     Mat d_simg = simg.clone();
     Mat d_timg = timg.clone();
