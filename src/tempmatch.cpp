eb6d31d3b948abd775d3f56e5c91204fd01480b6
hoshianaaa <hoshina54321@gmail.com>
2022-04-19 22:40:26 +0900

    UPDATE

--- src/tempmatch.cpp
+++ src/tempmatch.cpp
@@ -38,15 +38,17 @@ int main(int argc, const char * argv[])
     Mat d_simg = simg.clone();
     Mat d_timg = timg.clone();
 
-    int n = 2;
+    int pyrdown_num = 3;
 
-    for(int i=0;i<n;i++)
+    for(int i=0;i<pyrdown_num;i++)
     {
       cv::pyrDown( d_simg, d_simg, cv::Size( d_simg.cols/2, d_simg.rows/2 ));
       cv::pyrDown( d_timg, d_timg, cv::Size( d_timg.cols/2, d_timg.rows/2 ));
     }
 
-
+    double pyrdown_center_x = center_x / std::pow(2,pyrdown_num);
+    double pyrdown_center_y = center_y / std::pow(2,pyrdown_num);
+    
     imshow("s image", simg );
     imshow("t image", timg );
 
